e1b166fa0c9449d2b771c72d5e9496f36d0eaacf
hajunsa@unidocs.com <hajunsa@chromium.org>
2017-06-04 16:44:05 +0900

    change name Package.swift

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Kitura-WebSocket",
+    name: "Kitura-WebSocket-for-ComicPal",
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 1, minor: 7),
         .Package(url: "https://github.com/IBM-Swift/BlueCryptor.git", majorVersion: 0, minor: 8)
